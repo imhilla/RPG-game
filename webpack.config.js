1fd81e5d25ebcb7e000f7195d20fe93360f5809e
imhilla <hillaryodhiambo282@gmail.com>
2020-10-01 13:59:22 +0300

    configure phaser

--- webpack.config.js
+++ webpack.config.js
@@ -27,12 +27,24 @@ module.exports = {
           'csv-loader',
         ],
       },
+      // {
+      //   test: /\.xml$/,
+
+      //   use: [
+      //     'xml-loader',
+      //   ],
+      // },
       {
-        test: /\.xml$/,
-        use: [
-          'xml-loader',
-        ],
-      },
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-object-rest-spread']
+          }
+        }
+      }
     ],
   },
 };
