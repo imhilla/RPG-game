5c0eacc3f0ea52edfba0869759c04da3764b8ca4
imhilla <hillaryodhiambo282@gmail.com>
2020-10-02 18:01:51 +0300

    refactor

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import 'phaser';
 import config from './Config/config';
-import GameScene from './Scenes/GameScene';
+import Model from './Model';
 import BootScene from './Scenes/BootScene';
-import WorldScene from './Scenes/WorldScene';
 import PreloaderScene from './Scenes/PreloaderScene';
 import TitleScene from './Scenes/TitleScene';
 import OptionsScene from './Scenes/OptionsScene';
 import CreditsScene from './Scenes/CreditsScene';
-import Model from './Model';
+import GameScene from './Scenes/GameScene';
+import WorldScene from './Scenes/WorldScene';
 
 
 class Game extends Phaser.Game {
--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -25,3 +25,20 @@ export default class BootScene extends Phaser.Scene {
     this.scene.start('WorldScene');
   }
 };
+
+// import 'phaser';
+// import logoImg from "../assets/logo.png";
+ 
+// export default class BootScene extends Phaser.Scene {
+//   constructor () {
+//     super('Boot');
+//   }
+ 
+//   preload () {
+//     this.load.image('logo', logoImg);
+//   }
+ 
+//   create () {
+//     this.scene.start('Preloader');
+//   }
+// };
